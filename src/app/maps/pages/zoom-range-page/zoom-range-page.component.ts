32eb9f419a85555c5a9a6e5ce6a8b8c8addb0c2f
Gaston Gallegos <ggallegos@mobydigital.com>
2023-07-28 15:29:24 -0300

    slider working
    user can interact with the map slider and the slider responds to zoom level set by the mouse scroll

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -7,18 +7,46 @@ import { Map } from 'mapbox-gl'
 })
 export class ZoomRangePageComponent implements AfterViewInit{
 
-  @ViewChild('map') divMap?: ElementRef
+  @ViewChild('map') divMap?: ElementRef;
+
+  public zoom: number = 10;
+  public map?: Map;
 
   ngAfterViewInit(): void {
 
     if (!this.divMap) throw 'El elemento HTML no fue encontrado';
 
-    const map = new Map({
+    this.map = new Map({
       container: this.divMap?.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [-64.189767,-31.416321],
-      zoom: 9,
+      zoom: this.zoom,
     });
+    this.mapListeners()
   }
+mapListeners (){
+  if(!this.map) throw 'Mapa no inicializado';
+  this.map.on('zoom', (ev)=>{
+    this.zoom = this.map!.getZoom();
+  });
+
+  this.map.on('zoomend', (ev) =>{
+    if(this.map!.getZoom()<18) return;
+    this.map!.zoomTo(18);
+  })
+}
+
+zoomIn(){
+  this.map?.zoomIn();
+}
+
+zoomOut(){
+  this.map?.zoomOut();
+}
+
+zoomChanged( value: string){
+  this.zoom = Number(value);
+  this.map!.zoomTo( this.zoom );
+}
 
 }
