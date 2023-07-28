030aa3496eded997d32eaf2059dd0d2e83630653
Gaston Gallegos <ggallegos@mobydigital.com>
2023-07-28 16:52:24 -0300

    properties minimap
    properties now have an interactive minimap which can be moved

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import {Map, Marker} from 'mapbox-gl';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrls: ['./mini-map.component.css']
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit{
+
+  @Input() lngLat?: [number, number];
+  @ViewChild('map') divMap?: ElementRef;
+
+  ngAfterViewInit(): void {
+    if ( !this.divMap?.nativeElement)
+    if( !this.lngLat ) throw 'LngLat cannot be null';
+
+    const map = new Map({
+      container: this.divMap?.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.lngLat,
+      zoom: 15,
+    });
+
+    new Marker()
+    .setLngLat( this.lngLat! )
+    .addTo( map );
+  }
+
 
 }
